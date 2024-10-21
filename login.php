31609088e0be644ea990e9fe207c39fd2e2251ff
Revati Natu <124581682+Revati-N@users.noreply.github.com>
2024-10-21 12:26:47 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,17 +1,24 @@
 <?php
 include 'db_connect.php';
 
-$username = $_POST['username'];
+$identifier = $_POST['username']; // Renamed variable for clarity
 $password = $_POST['password'];
 
-$sql = "SELECT * FROM users WHERE username = '$username'";
+// Check if the input is an email
+if (filter_var($identifier, FILTER_VALIDATE_EMAIL)) {
+    $sql = "SELECT * FROM users WHERE email = '$identifier'";
+} else {
+    $sql = "SELECT * FROM users WHERE username = '$identifier'";
+}
+
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
     if (password_verify($password, $row['password'])) {
-        setcookie("user", $username, time() + (86400 * 30), "/");
+        setcookie("user", $row['username'], time() + (86400 * 30), "/");
         header("Location: dashboard.php");
+        exit();
     } else {
         echo "Incorrect password";
     }
